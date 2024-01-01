9fa55c1c0771ed8f724428e4788df03cb9c7f856
l-kaushik <contact.lokeshkaushik@gmail.com>
2024-01-01 17:56:33 +0530

    Added Basic Functions 1. Added peek, pop, push, isEmpty and a single value initialize constructor

--- cpp/Stack.h
+++ cpp/Stack.h
@@ -10,21 +10,61 @@ private:
     Node<T> *top{nullptr};
 
 public:
-    //constructor and destructor
+    // constructor and destructor
     Stack();
     Stack(T value);
     Stack(std::initializer_list<T> list);
 
-    //Functions
+    // Functions
     void push(T value);
     void pop();
     void peek();
+    bool isEmpty();
 
-    //Operator overloading
-    Stack& operator=(Stack& other);
-
+    // Operator overloading
+    Stack &operator=(Stack &other);
 };
 
 //< -------------------- METHOD DEFINITION -------------------->
 
+// constructor and destructor
+template <typename T>
+Stack<T>::Stack() : top(nullptr) {}
+
+template <typename T>
+Stack<T>::Stack(T value) : top(new Node<T>(value)) {}
+
+// Functions
+template <typename T>
+void Stack<T>::push(T value)
+{
+    if (!isEmpty())
+    {
+        Node<T> *temp = new Node<T>(value);
+        temp->setNextNode(top);
+        top = temp;
+    }
+    else
+        top = new Node<T>(value);
+}
+
+template <typename T>
+void Stack<T>::pop()
+{
+    if (isEmpty())
+    {
+        std::cout<<"Stack is already empty\n";
+    }
+    else{
+        Node<T> *temp = top;
+        top = top->getNextNode();
+        delete temp;
+    }
+}
+
+template <typename T>
+void Stack<T>::peek() { std::cout << top->getValue(); }
+
+template <typename T>
+bool Stack<T>::isEmpty() { return top == nullptr; }
 #endif
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,6 +1,19 @@
-#include "Node.h"
+#include "Stack.h"
 
 int main()
 {
+    Stack<int> a(1);
+   a.push(2);
+   a.push(3);
+   a.push(4);
+   a.push(5);
+   a.push(6);
+
+   while(!a.isEmpty())
+   {
+        a.peek();
+        std::cout<<'\n';
+        a.pop();
+   }
     return 0;
 }
