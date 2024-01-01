7fbc0207b3cac6e9e4226c6f733ad70c413a1334
l-kaushik <contact.lokeshkaushik@gmail.com>
2024-01-01 17:38:52 +0530

    using singly node 1. Separated doubly node and singly node. 2. using singly node for implementation. 3. class structure defined.

--- cpp/Stack.h
+++ cpp/Stack.h
@@ -7,7 +7,24 @@ template <typename T>
 class Stack
 {
 private:
+    Node<T> *top{nullptr};
+
+public:
+    //constructor and destructor
+    Stack();
+    Stack(T value);
+    Stack(std::initializer_list<T> list);
+
+    //Functions
+    void push(T value);
+    void pop();
+    void peek();
+
+    //Operator overloading
+    Stack& operator=(Stack& other);
 
 };
+
+//< -------------------- METHOD DEFINITION -------------------->
 
 #endif
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -2,16 +2,5 @@
 
 int main()
 {
-    Node<int> c(6);
-    Node<int> a(5,c);
-    Node<int> b(4,a);
-
-    a.setValue(56);
-    // b.getNextNode()->setNextNode(&a);
-    
-    b.display();
-    b.getNextNode()->display();
-    b.getNextNode()->getNextNode()->display();
-
     return 0;
 }
